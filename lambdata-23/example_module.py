e26d98de5e578007dc0de5272f1cf58105a834e8
Keila Yazdani <keila.yazdani@gmail.com>
2021-03-09 07:48:50 -0800

    First attempt at making packages

---
+++ lambdata-23/example_module.py
@@ -0,0 +1,18 @@
+'''Lambdata - a collectin of Data Science helper functions. '''
+
+# accessing libraries through pipenv
+import pandas as pd
+import numpy as np
+
+
+FAVORITE_NUMBERS =  [33, 56, 67, 190, 314]
+
+
+def df_cleaner(df):
+    '''Cleans a DF'''
+    # TODO - implement df_cleaner
+    pass
+
+def df_destroyer(df):
+    '''Destroys DF'''
+    pass
