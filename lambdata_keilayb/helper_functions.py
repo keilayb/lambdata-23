dfa0192d9716ba49fdb9ec033efcf4e211953442
Keila Yazdani <keila.yazdani@gmail.com>
2021-03-11 11:00:42 -0800

    fixing up some errors in building package

--- lambdata_keilayb/helper_functions.py
+++ lambdata_keilayb/helper_functions.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pandas as pd
+from sklearn.utils import shuffle
 
 
 class Data:
@@ -11,25 +13,38 @@ class Data:
             self.data = pd.Series(data)
         elif (type(data) == str) & (".csv" in data):
             self.data = pd.read_csv(data)
+        elif type(data) == pd.core.frame.DataFrame:
+            self.data = data
 
     def null_count(self):
         if type(self.data) == pd.core.series.Series:
-            df = pd.DataFrame({"data": self.data})
+            df = pd.DataFrame(self.data)
         elif type(self.data) == pd.core.frame.DataFrame:
             df = self.data
         null_values = df.isnull().sum().sum()
         return null_values
 
     def randomize(self, seed):
-        # if type(self.data) == pd.core.series.Series:
-        #
-        # if type(self.data) == pd.core.frame.DataFrame:
-        #
-        # return df
-        pass
+        if type(self.data) == pd.core.series.Series:
+            df = pd.DataFrame(self.data)
+        if type(self.data) == pd.core.frame.DataFrame:
+            df = self.data
+        shuffle1 = shuffle(df, random_state=seed)
+        cols = df.columns
+        for col in cols:
+            shuffle1[col] = np.random.permutation(shuffle1[col])
+        return shuffle1
 
 
 # Code to test whether functions work
 # test_list = [228, 8902, 563, 2, 67]
 # test_datalist = Data(test_list)
-# print(test_datalist.data)
+# print(test_datalist.randomize(2))
+#
+# test_df = pd.DataFrame({'A': [1, 2, 3, 4],
+#                         'B': [5, 6, 7, 8],
+#                         'C': [9, 10, 11, 12]})
+#
+# df_data = Data(test_df)
+#
+# # print(df_data.randomize(3))
